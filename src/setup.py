b6ddf2ead659a9325191a1a5acbdc709582b0757
Abibullaev <a.alim2019@mail.ru>
2023-04-26 10:19:29 +0300

    setup JWTManager

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 from image_service.bp import image_service_bp
 
 app = Flask(__name__)
+
 app.config.from_pyfile('config.py')
 
+jwt = JWTManager(app)
 
 app.register_blueprint(image_service_bp)
 
