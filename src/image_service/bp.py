e19f7accf1276e72ab272486e36657e382e01062
Abibullaev <a.alim2019@mail.ru>
2023-04-26 10:26:16 +0300

    create api and add resource DownloadLinkResource

--- src/image_service/bp.py
+++ src/image_service/bp.py
@@ -1,3 +1,10 @@
 from flask import Blueprint
+from flask_restful import Api
 
-image_service_bp = Blueprint('image_service', __name__, url_prefix='/image-service/api/v1')
+from image_service.resources import DownloadLinkResource
+
+image_service_bp = Blueprint('image_service', __name__, url_prefix='/image-service/api/v1/')
+
+api_bp = Api(image_service_bp)
+
+api_bp.add_resource(DownloadLinkResource, 'download_link')
