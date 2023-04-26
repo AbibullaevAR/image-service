63e8d4948317bbb1b9c278fbda9cdee204a2eb38
Abibullaev <a.alim2019@mail.ru>
2023-04-26 10:46:37 +0300

    add_command

--- src/image_service/bp.py
+++ src/image_service/bp.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 from flask_restful import Api
 
+from image_service.commands import cli_group
 from image_service.resources import DownloadLinkResource
 
 image_service_bp = Blueprint('image_service', __name__, url_prefix='/image-service/api/v1/')
+
+image_service_bp.cli.add_command(cli_group)
 
 api_bp = Api(image_service_bp)
 
