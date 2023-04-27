f9f46b584e66f521fdd2002b386675a58b79edda
Abibullaev <a.alim2019@mail.ru>
2023-04-27 16:58:03 +0300

    add resource DownloadFileResource

--- src/image_service/bp.py
+++ src/image_service/bp.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 
 from image_service.commands import cli_group
-from image_service.resources import UploadLinkResource, UploadFileResource
+from image_service.resources import UploadLinkResource, UploadFileResource, DownloadFileResource
 
 image_service_bp = Blueprint('image_service', __name__, url_prefix='/image-service/api/v1/')
 
@@ -12,3 +12,4 @@ api_bp = Api(image_service_bp)
 
 api_bp.add_resource(UploadLinkResource, 'upload_link/<string:name>/')
 api_bp.add_resource(UploadFileResource, 'upload_file/<string:token>/')
+api_bp.add_resource(DownloadFileResource, 'download_file/<string:name>/')
