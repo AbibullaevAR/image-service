d7867f07b6dee0b0ecf581c72ada54d3930f9033
Abibullaev <a.alim2019@mail.ru>
2023-04-27 16:57:43 +0300

    add UPLOAD_FOLDER

--- src/config.py
+++ src/config.py
@@ -1,11 +1,15 @@
 import os
+from pathlib import Path
 from datetime import timedelta
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+BASE_DIR = Path(__file__).resolve().parent
+
 DEBUG = bool(os.environ.get('DEBUG'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=36500)
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads/')
