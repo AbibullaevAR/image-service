20f1ae94754476a037b55a7040532dbdc2d8c3a7
Abibullaev <a.alim2019@mail.ru>
2023-04-27 16:19:28 +0300

    add resource for upload_link upload_file

--- src/image_service/bp.py
+++ src/image_service/bp.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 
 from image_service.commands import cli_group
-from image_service.resources import UploadLinkResource
+from image_service.resources import UploadLinkResource, UploadFileResource
 
 image_service_bp = Blueprint('image_service', __name__, url_prefix='/image-service/api/v1/')
 
@@ -10,4 +10,5 @@ image_service_bp.cli.add_command(cli_group)
 
 api_bp = Api(image_service_bp)
 
-api_bp.add_resource(UploadLinkResource, 'upload_link')
+api_bp.add_resource(UploadLinkResource, 'upload_link/<string:name>/')
+api_bp.add_resource(UploadFileResource, 'upload_file/<string:token>/')
