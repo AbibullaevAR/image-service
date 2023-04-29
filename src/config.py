5cbefc1d923df5abdb61df8f5a5334219464c138
Abibullaev <a.alim2019@mail.ru>
2023-04-29 21:02:55 +0300

    add MAX_CONTENT_LENGTH

--- src/config.py
+++ src/config.py
@@ -14,3 +14,4 @@ JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=36500)
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads/')
 BASE_DOWNLOAD_HOST = os.environ.get('BASE_DOWNLOAD_HOST')
+MAX_CONTENT_LENGTH = 5 * 1024 * 1024
