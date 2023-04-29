0f63cddf65496ca2ba10fff0476e0e2349cfa67b
Abibullaev <a.alim2019@mail.ru>
2023-04-29 13:18:03 +0300

    add DownloadLinkResource and change DownloadFileResource url

--- src/image_service/bp.py
+++ src/image_service/bp.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 
 from image_service.commands import cli_group
-from image_service.resources import UploadLinkResource, UploadFileResource, DownloadFileResource
+from image_service.resources import UploadLinkResource, UploadFileResource, DownloadFileResource, DownloadLinkResource
 
 image_service_bp = Blueprint('image_service', __name__, url_prefix='/image-service/api/v1/')
 
@@ -12,4 +12,5 @@ api_bp = Api(image_service_bp)
 
 api_bp.add_resource(UploadLinkResource, 'upload_link/<string:name>/')
 api_bp.add_resource(UploadFileResource, 'upload_file/<string:token>/')
-api_bp.add_resource(DownloadFileResource, 'download_file/<string:name>/')
+api_bp.add_resource(DownloadLinkResource, 'download_link/<string:name>/')
+api_bp.add_resource(DownloadFileResource, 'download_file/<string:token>/')
