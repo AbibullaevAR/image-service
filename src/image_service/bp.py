26cf259dce592cb9b41e0eb3b545d670d1db428e
Abibullaev <a.alim2019@mail.ru>
2023-04-29 15:02:39 +0300

    add DeleteFileResource

--- src/image_service/bp.py
+++ src/image_service/bp.py
@@ -2,7 +2,13 @@ from flask import Blueprint
 from flask_restful import Api
 
 from image_service.commands import cli_group
-from image_service.resources import UploadLinkResource, UploadFileResource, DownloadFileResource, DownloadLinkResource
+from image_service.resources import (
+    UploadLinkResource, 
+    UploadFileResource, 
+    DownloadFileResource, 
+    DownloadLinkResource, 
+    DeleteFileResource
+    )
 
 image_service_bp = Blueprint('image_service', __name__, url_prefix='/image-service/api/v1/')
 
@@ -14,3 +20,4 @@ api_bp.add_resource(UploadLinkResource, 'upload_link/<string:name>/')
 api_bp.add_resource(UploadFileResource, 'upload_file/<string:token>/')
 api_bp.add_resource(DownloadLinkResource, 'download_link/<string:name>/')
 api_bp.add_resource(DownloadFileResource, 'download_file/<string:token>/')
+api_bp.add_resource(DeleteFileResource, 'delete_file/<string:name>/')
