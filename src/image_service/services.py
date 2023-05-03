1a061cbc77be842327568b0cfbc69054e137d62d
AbibullaevAR <a.alim2019@mail.ru>
2023-05-03 12:25:44 +0300

    create optimize_image

--- src/image_service/services.py
+++ src/image_service/services.py
@@ -29,4 +29,21 @@ def validate_image(file: BytesIO, file_name: str):
     
     return img
 
+def optimize_image(img: Image) -> Image:
+    # Получаем ширину и высоту изображения
+    width, height = img.size
 
+    # Устанавливаем максимальную ширину и высоту
+    max_width = 600
+    max_height = 600
+
+    # Вычисляем новые размеры изображения
+    if width > max_width or height > max_height:
+        ratio = min(max_width / width, max_height / height)
+        new_width = round(width * ratio)
+        new_height = round(height * ratio)
+
+        # Сжимаем изображение
+        return img.resize((new_width, new_height), Image.ANTIALIAS)
+    
+    return img
