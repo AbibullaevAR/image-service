11a7f678b5a8566376332f94b255d3276acd8ae6
AbibullaevAR <a.alim2019@mail.ru>
2023-05-03 11:14:12 +0300

    add validate to name format file format

--- src/image_service/services.py
+++ src/image_service/services.py
@@ -7,7 +7,7 @@ class ImgNotValid(Exception):
         self.message = message
 
 
-def validate_image(file: BytesIO):
+def validate_image(file: BytesIO, file_name: str):
     # Проверяем, является ли файл изображением
     try:
         img = Image.open(file)
@@ -17,5 +17,8 @@ def validate_image(file: BytesIO):
     # Проверяем формат изображения
     if img.format not in ['JPEG', 'PNG', 'JPG']:
         raise ImgNotValid('Недопустимый формат изображения. Разрешены только JPEG, PNG и JPG')
-
+    
+    if not file_name.endswith(img.format.lower()):
+        raise ImgNotValid('Формат изображения не совпадает с форматом в названии')
+    
     return img
