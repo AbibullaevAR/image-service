f72abb35e29a5c99b237162f4dfb27816ff6a7c2
AbibullaevAR <a.alim2019@mail.ru>
2023-05-09 11:07:15 +0300

    delete optimaze image set JPG as avl format

--- src/image_service/services.py
+++ src/image_service/services.py
@@ -7,12 +7,7 @@ class ImgNotValid(Exception):
         self.message = message
 
 
-def validate_image(file: BytesIO, file_name: str):
-
-    available_formats = {
-        'JPEG': ['.jpg', '.jpeg'],
-        'PNG': ['.png']
-    }
+def validate_image(file: BytesIO):
 
     # Проверяем, является ли файл изображением
     try:
@@ -21,28 +16,8 @@ def validate_image(file: BytesIO, file_name: str):
         raise ImgNotValid('Файл не является изображением')
 
     # Проверяем формат изображения
-    if img.format not in ['JPEG', 'PNG']:
-        raise ImgNotValid('Недопустимый формат изображения. Разрешены только JPEG, PNG')
-
-    if not file_name.endswith(tuple(available_formats[img.format])):
-        raise ImgNotValid('Формат изображения не совпадает с форматом в названии')
+    if img.format not in ['JPEG', 'PNG', 'JPG']:
+        raise ImgNotValid('Недопустимый формат изображения. Разрешены только JPEG, PNG, JPG')
     
     return img
 
-def optimize_image(img: Image) -> Image:
-    # Получаем ширину и высоту изображения
-    width, height = img.size
-
-    # Устанавливаем максимальную ширину и высоту
-    max_width = 550
-    max_height = 470
-    
-    # Не применяем сжатие если размеры меньше максимальных
-    if width < max_width or height < max_height:
-        return img
-
-    # Расчет новых размеров с сохранением соотношения сторон
-    if width < height:
-        return img.resize((max_width, int((max_width / width) * height)), Image.LANCZOS)
-
-    return img.resize((int((max_height / height) * width), max_height), Image.LANCZOS)
